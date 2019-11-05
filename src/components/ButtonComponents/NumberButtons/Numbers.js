8ff2be351773c0550f3aa960075ee989cc596d93
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-05 17:41:29 -0500

    convert raw numbers data to object (like operators)

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -3,7 +3,14 @@ import React from "react";
 // import any components needed
 import NumberButton from "./NumberButton";
 // import your array data to from the provided data file
-import { numbers as numbersData } from "../../../data";
+import { numbers as numbersRawData } from "../../../data";
+
+const numbersData = numbersRawData.map (
+  (el) => ({
+    "char"  : el,
+    "value" : el,
+  })
+);
 
 const Numbers = (props) => {
   // STEP 2 - add the imported data to state
@@ -12,7 +19,7 @@ const Numbers = (props) => {
   return (
     <div className="button-group numbers">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button component matching the name on the provided file. Pass it any props needed by the child component. */}
-      
+
     </div>
   );
 };
