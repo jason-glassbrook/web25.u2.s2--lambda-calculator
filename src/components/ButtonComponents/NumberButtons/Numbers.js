954963d99dad4ff9d6281b5a24b1b8399e2aaffb
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-05 17:30:32 -0500

    redo nested comment

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -10,9 +10,9 @@ const Numbers = (props) => {
   const [numbers , setNumbers] = React.useState (numbersData);
   //
   return (
-    <div>{
-      /* STEP 3 - Use .map() to iterate over your array data and return a button component matching the name on the provided file. Pass it any props needed by the child component. */
-    }</div>
+    <div>
+      {/* STEP 3 - Use .map() to iterate over your array data and return a button component matching the name on the provided file. Pass it any props needed by the child component. */}
+    </div>
   );
 };
 
