c7b54e686bb7164175ea100a643e8a383e66ffc9
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-05 17:04:44 -0500

    create HTML button element

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-const NumberButton = () => {
+const NumberButton = (props) => {
   return (
-    <>
+    <button className="number-button" data-value={props.value} onClick={props.onClick}>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.char}
+    </button>
   );
 };
