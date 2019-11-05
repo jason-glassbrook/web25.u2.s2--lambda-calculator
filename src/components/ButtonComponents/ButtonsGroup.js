899203b03115a92f752423497d36f649fac8235e
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-05 18:01:30 -0500

    fix: remove unnecessary state from ButtonsGroup

--- src/components/ButtonComponents/ButtonsGroup.js
+++ src/components/ButtonComponents/ButtonsGroup.js
@@ -1,8 +1,6 @@
 import React from "react";
 
 const ButtonsGroup = (props , children) => {
-  const [specials , setSpecials] = React.useState (specialsData);
-  //
   return (
     <div className={`buttons-group ${props.name}`}>
       {children}
