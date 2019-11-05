0cea64eda9e623c91ee5aeed7085f26a6999e10e
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-05 17:28:32 -0500

    add components to App

--- src/App.js
+++ src/App.js
@@ -22,6 +22,10 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display value={0}/>
+        <Specials/>
+        <Numbers/>
+        <Operators/>
       </div>
     </div>
   );
