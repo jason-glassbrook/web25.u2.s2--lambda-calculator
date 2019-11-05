2f4ac9666453f396fb197e7ca4990f2e237c072e
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-05 16:57:28 -0500

    make comments nicer

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -10,10 +10,8 @@ const Numbers = () => {
   const [numbers , setNumbers] = React.useState (numbersData);
   //
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
-    </div>
+    <div>{
+      /* STEP 3 - Use .map() to iterate over your array data and return a button component matching the name on the provided file. Pass it any props needed by the child component. */
+    }</div>
   );
 };
--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -10,10 +10,8 @@ const Operators = () => {
   const [operators , setOperators] = React.useState (operatorsData);
   //
   return (
-    <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
-    </div>
+    <div>{
+      /* STEP 3 - Use .map() to iterate over your array data and return a button component matching the name on the provided file. Pass it any props needed by the child component. */
+    }</div>
   );
 };
