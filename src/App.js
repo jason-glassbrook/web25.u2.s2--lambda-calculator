dbfaa911534f70b376ccda3cf7d12df6106a4fbc
o1123516766542617 <o1123516766542617@outlook.com>
2019-11-05 20:27:36 -0500

    create initial state for calculator

--- src/App.js
+++ src/App.js
@@ -11,12 +11,27 @@ import Numbers from "./components/ButtonComponents/NumberButtons/Numbers";
 import Operators from "./components/ButtonComponents/OperatorButtons/Operators";
 import Specials from "./components/ButtonComponents/SpecialButtons/Specials";
 
+// initial state
+const initState = {
+  "actions" : [
+    {
+      "type"  : "init",
+      "char"  : "0",
+      "value" : 0,
+    }
+  ],
+  "values" : [
+    0
+  ],
+}
+
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
   // Once the state hooks are in place write some functions to hold data in state and update that data depending on what it needs to be doing
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
+  const [state , setState] = React.useState (initState);
 
   return (
     <div className="container">
